4767e8d8fc5a898f1e0b2232f9640646432a05c8
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-02-14 12:37:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//create form submit event
+//add function to move user input to ul todo-list
+    //no todo when blank or spaces
+//item added must be a button nested inside a li for accessibility
+//return input to empty string
+//when item clicked, text-decoration adds a strikethrough (clicked once)
+//when stricken item clicked, remove from list (clicked twice)
+//be able to add multiple items from list
+//be able to remove items from middle of list
