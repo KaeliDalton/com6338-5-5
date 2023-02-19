fc214eb15be9447355028d8565bb99635a2251f8
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-02-19 11:44:07 -0500

    create form submit

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 var form = document.getElementById("add-todo")
 var list = document.getElementById("todo-list")
 var input = document.querySelector('input')
+
+//create form submit event
+form.onsubmit = function(e){
+    e.preventDefault()
+    var inputValue = document.querySelector('input').value
+    
+}
+
